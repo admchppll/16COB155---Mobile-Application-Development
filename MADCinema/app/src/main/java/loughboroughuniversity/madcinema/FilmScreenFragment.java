eb18a75b9f85a9fdd225a9100575ae28551571b5
DarrenBrooks <darren.brooks41@gmail.com>
2017-04-13 12:52:54 +0100

    Application Creation
    Homepage: home_screen_layout.xml and HomeScreenFragment.java.
    
    Film Screen: film_layout.xml and FilmScreenFragment.java
    
    Film List Screen: films_list_layout.xml and FilmListScreenFragment.java
    
    Location Screen: location_layout.xml and LocationScreenFragment.java
    
    Timetable Screen: timetable_layout.xml and TimetableScreenFragment.java
    
    Home Activity is the only activity, all other screens are known as
    fragments. All other layout files don't need to be touched for now as
    they help to create the menu.

---
+++ MADCinema/app/src/main/java/loughboroughuniversity/madcinema/FilmScreenFragment.java
@@ -0,0 +1,25 @@
+package loughboroughuniversity.madcinema;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by darre_000 on 13/04/2017.
+ */
+
+public class FilmScreenFragment extends Fragment {
+    View myView;
+
+    @Nullable
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        myView = inflater.inflate(R.layout.film_layout, container, false);
+
+        return myView;
+
+    }
+}
