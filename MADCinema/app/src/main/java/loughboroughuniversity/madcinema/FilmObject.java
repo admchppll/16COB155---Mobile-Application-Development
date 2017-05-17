c592e410f21754ce96f1dbfd72efb6c0b1a3cf25
jithinr77 <jithinr77@gmail.com>
2017-05-17 19:22:39 +0100

    Created Film Object

---
+++ MADCinema/app/src/main/java/loughboroughuniversity/madcinema/FilmObject.java
@@ -0,0 +1,16 @@
+package loughboroughuniversity.madcinema;
+
+/**
+ * Created by Jithin on 17/05/2017.
+ */
+
+public class FilmObject {
+    int id;
+    String dame;
+    String description;
+    int rating;
+    int age;
+    String trailer;
+    String img;
+    String added;
+}
