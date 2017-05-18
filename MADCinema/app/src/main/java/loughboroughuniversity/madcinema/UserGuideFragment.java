484820f59debac391dd616fd4b9819e4bde66493
Adam Chappell <adam@adamchappell.com>
2017-05-18 04:17:08 +0100

    Added user guide web view
    -Added UserGuide.html to source code
    --Page is incomplete but is a start. Shouldn't take long to pad out with
    additional content and css styling
    Web View created and configured
    -Enabled javascript

--- MADCinema/app/src/main/java/loughboroughuniversity/madcinema/UserGuideFragment.java
+++ MADCinema/app/src/main/java/loughboroughuniversity/madcinema/UserGuideFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 /**
  * Created by darre_000 on 17/05/2017.
@@ -14,6 +16,15 @@ public class UserGuideFragment extends Fragment {
     View myView;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.user_guide_layout, container, false);
+
+        //load page
+        WebView userGuide = (WebView) myView.findViewById(R.id.webUserGuide);
+        userGuide.loadUrl("http://www.ac-portal.uk/mad/UserGuide.html");
+
+        //enable javascript
+        WebSettings webSettings = userGuide.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
         return myView;
     }
 }
